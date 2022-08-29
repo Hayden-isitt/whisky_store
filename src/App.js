05fb1e4f5f5e8df8d2b950eeae081946838307ae
hayden-isitt <hayden.isitt@hotmail.com>
2022-08-29 19:57:10 +1200

    Basic layout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from 'react';
+import { Outlet, Link } from "react-router-dom";
+import AppBar from '@mui/material/AppBar';
+import { Avatar, Box, Container, Toolbar, Typography } from '@mui/material';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    const NavLink = (props) => {
+        return (
+            <>
+                <Typography variant='h6' color='white'>
+                    <Link className='App-navlink' style={{ textDecoration: 'none' }} to={'/' + props.link}>
+                        {props.name}
+                    </Link>
+                </Typography>
+            </>
+        )
+    }
+
+    return (
+    <Box id="root">
+        <AppBar position='sticky' sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}>
+            <Box sx={{ display: 'flex', flexDirection: 'column'}}>
+                <Typography variant='h3'>Kilkimi Whiskies</Typography>
+                <Toolbar>
+                    <NavLink name='Shop' link='shop'></NavLink>
+                    <NavLink name='My Cart' link='cart'></NavLink>
+                </Toolbar>
+            </Box>
+        </AppBar>
+        <Box sx={{ display: 'flex', justifyContent: 'center'}}>
+            <Outlet/>
+        </Box>
+    </Box>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,12 +3,46 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createTheme } from "@mui/material"
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { ThemeProvider } from '@mui/material';
+import Shop from './Shop'
+
+const theme = createTheme({
+  spacing: 4,
+  palette: {
+      primary: {
+        main: '#22396f',
+        light: '#53639e',
+        dark: '#001443',
+        contrastText: '#fff',
+      },
+      secondary: {
+        main: '#e0b869',
+        light: '#ffea98',
+        dark: '#ac883c',
+        contrastText: '#22396f',
+      },
+  },
+})
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+		<ThemeProvider
+		theme={theme}
+		>
+			<BrowserRouter>
+				<React.StrictMode>
+					<Routes>
+						<Route path="/" element={<App />}>
+							<Route path="/shop" element={<></>}/>
+							<Route path="/cart" element={<></>}/>
+						</Route>
+					</Routes>
+				</React.StrictMode>
+			</BrowserRouter>
+
+		</ThemeProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
