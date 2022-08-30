9f5d9e0c8ed833d98af57316d3e82d5fe98203db
hayden-isitt <hayden.isitt@hotmail.com>
2022-08-30 14:56:43 +1200

    simplifies cart logic

--- src/Cart.js
+++ src/Cart.js
@@ -11,12 +11,25 @@ import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import StockData from './StockData';
 
+const calculateItemPrice = (productCode, qty, price) => {
+    
+    var totalPrice = 0
+
+    if(productCode == 'JOHN'){
+        totalPrice = price * (qty - Math.floor(qty / 4))
+    }else if(productCode == 'ARR' && qty >= 12){
+        totalPrice = 75 * qty
+    }else{
+        totalPrice = price * qty
+    }
+    return totalPrice
+}
+
 function Cart() {
     const [cartData, setCartData] = useState(JSON.parse(localStorage.getItem('cart')) ?? {})
     const [allPrices, ] = useState(Object.fromEntries(new Map(StockData.map((x) => [x.sku, x.price]))))
-    const [totalPrice, setTotalPrice] = useState(0)
-
-    const calculatePrice = (cart) => {
+    
+    const calculateTotalPrice = (cart) => {
         var totalPrice = 0
         var currProduct = null
         var currQty = 0
@@ -25,28 +38,13 @@ function Cart() {
         for(var i=0; i < Object.keys(cart).length; i+=1){
             currProduct = Object.keys(cart)[i]
             currQty = cart[currProduct]
-            
-            console.log(currProduct, currQty)
-            if(currProduct == 'JOHN'){
-                currPrice = allPrices[currProduct] * (currQty - Math.floor(currQty / 4))
-            }else if(currProduct == 'ARR' && currQty >= 12){
-                currPrice = 75 * currQty
-            }else{
-                currPrice = allPrices[currProduct] * currQty
-            }
-            totalPrice += currPrice
+            currPrice = allPrices[currProduct]
+
+            totalPrice += calculateItemPrice(currProduct, currQty, currPrice)
         }
         return totalPrice
     }
-
-    const updateCart = (productCode, quantity) => {
-        var currCart = cartData
-        currCart[productCode] = parseInt(quantity)
-        
-        setCartData(currCart)
-        localStorage.setItem('cart', JSON.stringify(currCart))
-        setTotalPrice(calculatePrice(currCart))
-    }
+    const [totalPrice, setTotalPrice] = useState(calculateTotalPrice(cartData))
 
     return (
         <Box>
@@ -60,7 +58,7 @@ function Cart() {
                     </TableRow>
                     </TableHead>
                     <TableBody>
-                        {Object.entries(cartData).map((row, key) => <Item key={key} item={row[0]} qty={row[1]} updateCart={updateCart}/>)}
+                        {Object.entries(cartData).map((row, key) => <Item key={key} item={row[0]} qty={row[1]}/>)}
                     </TableBody>
                 </Table>
             </TableContainer>
@@ -77,25 +75,14 @@ function Item(props) {
     const [itemData, ] = useState(StockData.filter(x => x.sku == props.item)[0])
     const [qty, setQty] = useState(props.qty)
 
-    useEffect(() => {
-        props.updateCart(props.item, qty)
-    }, [qty])
-
     return(
         <>
             <TableRow
             sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
             >
                 <TableCell align="left">{itemData.name}</TableCell>
-                <TableCell align="left">
-                    <TextField 
-                        type='number'
-                        value={qty}
-                        onChange={(e) => setQty(e.target.value)}
-                        size='small'
-                    />
-                </TableCell>
-                <TableCell align="left">{`$${(itemData.price * qty).toFixed(2)}`}</TableCell>
+                <TableCell align="left">{qty}</TableCell>
+                <TableCell align="left">{`$${(calculateItemPrice(itemData.sku, qty, itemData.price)).toFixed(2)}`}</TableCell>
             </TableRow>
         </>
     )
